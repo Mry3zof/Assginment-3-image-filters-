347122d30b1846a3218bec06849c9ad61fd32ddf
Aldwatlly <youssef2005aldwaltly@gmail.com>
2024-03-27 00:05:18 +0200

    Created filters<3

---
+++ filters1,2,4,5,7,10.cpp
@@ -0,0 +1,236 @@
+using namespace std;
+#include "Image_Class.h"
+#include<cmath>
+void FlipHorizontal(string s){
+    Image image(s);
+    int f=image.width-1;
+    Image image2(image.width,image.height);
+
+    for (int i = 0; i < image.width; ++i) {
+        for (int j = 0; j < image.height; ++j) {
+            unsigned  int avg = 0;
+
+            for (int k = 0; k < 3; ++k) {
+                image2(f,j,k)=image(i,j,k);
+            }
+        }
+        f--;
+    }
+
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    image2.saveImage("new.png");
+
+}
+void FlidVertical(string s){
+    Image image(s);
+    Image image2(image.width,image.height);
+    int y;
+    for (int i = 0; i < image.width; ++i) {
+        y=0;
+        for (int j = image.height-1; j >=0; --j) {
+            for (int k = 0; k < 3; ++k) {
+                image2(i,y,k)=image(i,j,k);
+            }
+            y++;
+        }
+    }
+
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    image2.saveImage("new.jpg");
+}
+void GrayScaleConversion(string s){
+    Image image(s);
+    for (int i = 0; i < image.width; ++i) {
+        for (int j = image.height-1; j >=0; --j) {
+            unsigned avg=0;
+            for (int k = 0; k < 3; ++k) {
+                avg+=image(i,j,k);
+            }
+            avg/=3;
+            image(i,j,0)=avg;
+            image(i,j,1)=avg;
+            image(i,j,2)=avg;
+
+        }
+    }
+
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    image.saveImage("new.jpg");
+}
+void DarkenImage(string s){
+    Image image("building.jpg");
+    for (int i = 0; i < image.width; ++i) {
+        for (int j = image.height-1; j >=0; --j) {
+            unsigned avg=0;
+            for (int k = 0; k < 3; ++k) {
+                if(image(i,j,k)<=124){
+                    image(i,j,k)*=2;
+                }
+                else{
+                    image(i,j,k)=255;
+                }
+            }
+        }   
+    }
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    image.saveImage("new.jpg");
+}
+void BrightenImage(string s){
+    Image image(s);
+    for (int i = 0; i < image.width; ++i) {
+        for (int j = image.height-1; j >=0; --j) {
+            unsigned avg=0;
+            for (int k = 0; k < 3; ++k) {
+                if(image(i,j,k)<=124){
+                    image(i,j,k)*=2;
+                }
+                else{
+                    image(i,j,k)=255;
+                }
+            }
+        }   
+    }
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    image.saveImage("new.jpg");
+}
+void BlackAndWhite(string s){
+    Image image(s);
+    
+   for (int i = 0; i < image.width; ++i) {
+        for (int j = image.height-1; j >=0; --j) {
+            unsigned avg=0;
+            for (int k = 0; k < 3; ++k) {
+                avg+=image(i,j,k);
+            }
+            avg/=3;
+            image(i,j,0)=avg;
+            image(i,j,1)=avg;
+            image(i,j,2)=avg;
+
+        }
+    }
+    for (int i =0 ; i<image.width;i++){
+        for(int j=0;j<image.height;j++){
+            unsigned x=0;
+            for(int k=0;k<3;k++){
+                x+=image(i,j,k);
+            }
+            if(x/3>128){
+                for(int k=0;k<3;k++){
+                    image(i,j,k)=255;
+                }
+            }
+            else {
+            for(int k=0;k<3;k++){
+                image(i,j,k)=0;
+            }
+            }
+        }
+    }
+    
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    image.saveImage("new.jpg");
+}
+void DetectImageEdges(string s) {
+    Image image(s);
+    Image image2(image.width, image.height);
+    for (int i = 0; i < image.width; ++i) {
+        int pixel1 = 0;
+        int pixel2 = 0;
+        unsigned count = 0;
+        for (int j = 0; j < image.height; ++j) {
+            for (int k = 0; k < image.channels; k++) {
+                pixel2 += image(i, j, k);
+            }
+            count++;
+            if (count == 1) {
+                pixel1 = pixel2;
+                pixel2 = 0;
+            }
+            else if ((count == 2) && (abs(pixel2 - pixel1) > 30)) {
+                image2(i, j, 0) = 0;
+                image2(i, j, 1) = 0;
+                image2(i, j, 2) = 0;
+                image2(i, j - 1, 0) = 255;
+                image2(i, j - 1, 1) = 255;
+                image2(i, j - 1, 2) = 255;
+                count = 0;
+                pixel1 = 0;
+                pixel2 = 0;
+            }
+            else {
+                image2(i, j, 0) = 255;
+                image2(i, j, 1) = 255;
+                image2(i, j, 2) = 255;
+                image2(i, j - 1, 0) = 255;
+                image2(i, j - 1, 1) = 255;
+                image2(i, j - 1, 2) = 255;
+                count = 0;
+                pixel1 = 0;
+                pixel2 = 0;
+            }
+        }
+    }
+    image2.saveImage("xx.jpg");
+}
+Image resizeImage(Image& image,int x,int  y) {
+    Image potatos(x, y);
+    for (int i = 0; i < potatos.width; ++i) {
+        for (int j = 0; j < potatos.height; ++j) {
+            for (int k = 0; k < potatos.channels; ++k) {
+                potatos(i, j, k) = image(i * image.width / potatos.width, j * image.height / potatos.height, k);
+
+            }
+        }
+    }
+    return potatos;
+}
+
+int main() {
+    Image image("luffy.jpg");
+    Image image2("toy2.jpg");
+    int x = max(image.width, image2.width);
+    int y = max(image.height, image2.height);
+    Image potatos(x, y);
+    for (int i = 0; i < potatos.width; ++i) {
+        for (int j = 0; j < potatos.height; ++j) {
+            for (int k = 0; k < potatos.channels; ++k) {
+                potatos(i, j, k) = image(i * image.width / potatos.width, j * image.height / potatos.height, k);
+
+            }
+        }
+    }
+    Image potatos2(x, y);
+    for (int i = 0; i < potatos2.width; ++i) {
+        for (int j = 0; j < potatos2.height; ++j) {
+            for (int k = 0; k < potatos2.channels; ++k) {
+                potatos2(i, j, k) = image2(i * image2.width / potatos.width, j * image2.height / potatos.height, k);
+
+            }
+        }
+    }
+    Image final(x, y);
+
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            for (int k = 0; k < 3; k++) {
+                final(i,j,k) = (potatos(i, j, k) + potatos2(i, j, k)) / 2;
+                
+            }
+        }
+    }
+    final.saveImage("new.jpg");
+    return 0;
+}
